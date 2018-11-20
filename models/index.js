54aa11655f0fd0d14a3e160806ec9356882870a1
Jessica Walters <jessica@Jessicas-MacBook-Pro-2.local>
2018-11-20 14:09:25 -0800

    add mlab db

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const MONGOOSE = require('mongoose');
 MONGOOSE.set('debug', true);
-MONGOOSE.connect('mongodb://localhost/todo-api', { useNewUrlParser: true });
+//MONGOOSE.connect('mongodb://localhost/todo-api', { useNewUrlParser: true });
+MONGOOSE.connect('mongodb://jessicakwalters:password8414@ds039017.mlab.com:39017/inspiretodo', { useNewUrlParser: true });
 
 MONGOOSE.Promise = Promise;
 
